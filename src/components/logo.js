f937ad0f81bb4b66e5cfac7b7330b9160223f6d6
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-08-01 22:20:59 +0300

    added some styles

---
+++ src/components/logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import SnakeLogo from '../img/snake.png'
+import '../App.css'
+
+export default (props) => {
+    return <img src={SnakeLogo} alt="logo" className="logo"/>
+}
--- src/components/panel.js
+++ src/components/panel.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Logo from './logo'
 import '../App.css'
 
 export default (props) => {
     return(
         <div className='panel'>
-
+            <Logo />
+            <div className="panel-text">Snake Gennady</div>
         </div>
     );
 }
